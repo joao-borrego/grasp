f84cbfb55dafdc04aeec2226296046fb52c4b154
Borrego <joaohenriqueborrego@gmail.com>
2018-04-02 18:21:05 +0100

    fix setup script

--- setup.sh
+++ setup.sh
@@ -3,12 +3,7 @@
 # Source Gazebo environment
 source /usr/local/share/gazebo-9/setup.sh
 
-# Export path to model meshes
-export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:`pwd`/hands
-# Export path to model meshes
-export GAZEBO_MEDIA_PATH=${GAZEBO_MEDIA_PATH}:`pwd`/hands
-# Export path to model meshes
-export GAZEBO_RESOURCE_PATH=${GAZEBO_RESOURCE_PATH}:`pwd`/hands
-
+# Export location of gazebo plugins
+export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:`pwd`/build/plugins
 
 echo "Successfully initialised."
