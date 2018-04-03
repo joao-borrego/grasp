95d426ff3c4a05f364399eb2c8124cf80f6e544d
Borrego <joaohenriqueborrego@gmail.com>
2018-04-03 12:32:41 +0100

    add basic features to plugin

--- plugins/GripperPlugin.cc
+++ plugins/GripperPlugin.cc
@@ -30,8 +30,8 @@ class GripperPluginPrivate
 GZ_REGISTER_MODEL_PLUGIN(GripperPlugin)
 
 /////////////////////////////////////////////////
-GripperPlugin::GripperPlugin()
-    : ModelPlugin(), dataPtr(new GripperPluginPrivate)
+GripperPlugin::GripperPlugin() : ModelPlugin(),
+    data_ptr(new GripperPluginPrivate)
 {
     gzmsg << "[GripperPlugin] Loaded gripper plugin." << std::endl;
 }
@@ -45,12 +45,72 @@ GripperPlugin::~GripperPlugin()
 /////////////////////////////////////////////////
 void GripperPlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
 {
-    // Safety check
-    if (_model->GetJointCount() == 0)
+    // Validate model to which plugin is attached
+    if (_model->GetJointCount() != 0)
+    {
+        this->model = _model;
+    }
+    else
     {
         gzerr << "[GripperPlugin] Invalid model." << std::endl;
         return;
     }
+    
+    // Extract parameters from SDF element
+
+    // Reference links
+    if (_sdf->HasElement(PARAM_BASE_LINK) && 
+        _sdf->HasElement(PARAM_LEFT_LINK) &&
+        _sdf->HasElement(PARAM_RIGHT_LINK))
+    {
+        std::string base_link_name = _sdf->Get<std::string>(PARAM_BASE_LINK);
+        std::string left_link_name = _sdf->Get<std::string>(PARAM_LEFT_LINK);
+        std::string right_link_name = _sdf->Get<std::string>(PARAM_RIGHT_LINK);
+        this->base_link = _model->GetLink(base_link_name);
+        this->left_link = _model->GetLink(left_link_name);
+        this->right_link = _model->GetLink(right_link_name);
+
+        if (!this->base_link || !this->left_link || !this->right_link)
+        {
+            gzerr << "[GripperPlugin] Invalid reference links specified." << std::endl;
+            return;       
+        }
+    }
+    else
+    {
+        gzerr << "[GripperPlugin] No reference links specified." << std::endl;
+        return;
+    }
+
+    // Enable/disable gravity
+    if (_sdf->HasElement(PARAM_GRAVITY))
+    {
+        bool gravity_enabled = _sdf->Get<bool>(PARAM_GRAVITY);
+        this->model->SetGravityMode(gravity_enabled);
+    }
+
+    // Connect to world update event
+    this->update_connection = event::Events::ConnectWorldUpdateBegin(
+        std::bind(&GripperPlugin::onUpdate, this));
+    
+    // Setup transport node
+    this->data_ptr->node = transport::NodePtr(new transport::Node());
+    this->data_ptr->node->Init();
+    // Subcribe to the monitored requests topic
+    this->data_ptr->sub = this->data_ptr->node->Subscribe(REQUEST_TOPIC,
+        &GripperPlugin::onRequest, this);
+}
+
+/////////////////////////////////////////////////
+void GripperPlugin::onUpdate()
+{
+
+}
+
+/////////////////////////////////////////////////
+void GripperPlugin::onRequest(GripperMsgPtr &_msg)
+{
+
 }
 
 }
--- plugins/GripperPlugin.hh
+++ plugins/GripperPlugin.hh
@@ -16,18 +16,57 @@
 #include <gazebo/physics/physics.hh>
 #include <gazebo/transport/transport.hh>
 
-namespace gazebo{
+// Custom messages
+#include "gripper.pb.h"
+
+namespace GripperPlugin {
+    
+    // Plugin parameters
+
+    /// Base link name parameter SDF
+    #define PARAM_BASE_LINK  "baseLink"
+    /// Left gripper link name parameter SDF
+    #define PARAM_LEFT_LINK  "leftLink"
+    /// Right gripper link name parameter SDF
+    #define PARAM_RIGHT_LINK "rightLink"
+    /// Set gravity state parameter SDF
+    #define PARAM_GRAVITY    "gravity"
+
+    // Plugin messages
+
+    /// Topic for incoming requests
+    #define REQUEST_TOPIC    "~/gripper"    
+}
+
+namespace gazebo {
+
+    /// Shared pointer declaration for request message type
+    typedef const boost::shared_ptr<const grasp::msgs::Gripper>
+        GripperMsgPtr;
 
     // Forward declaration of private data class
     class GripperPluginPrivate;
 
-    class GripperPlugin : public ModelPlugin {
-
+    class GripperPlugin : public ModelPlugin
+    {
         // Private attributes
 
         /// Class with private attributes
-        private: std::unique_ptr<GripperPluginPrivate> dataPtr;
+        private: std::unique_ptr<GripperPluginPrivate> data_ptr;
+        /// Model to which the plugin is attached
+        private: physics::ModelPtr model;
+        /// Connection to world update event
+        private: event::ConnectionPtr update_connection;
 
+        /// Model name
+        private: std::string model_name;
+        /// Base link pointer
+        private: physics::LinkPtr base_link;
+        /// Left gripper link pointer
+        private: physics::LinkPtr left_link;
+        /// Right gripper link pointer
+        private: physics::LinkPtr right_link;
+        
         // Protected attributes
 
         // Public methods
@@ -43,6 +82,12 @@ namespace gazebo{
         /// \param _sdf   The sdf element pointer
         public: virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
 
+        /// \brief Updates model on world update
+        public: void onUpdate();
+
+        /// \brief Callback function for handling incoming requests
+        /// \param _msg  The message
+        public: void onRequest(GripperMsgPtr & _msg);
     };
 }
 
