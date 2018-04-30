3b4561b727849305aef8f41c81a536031bbc433f
Borrego <joaohenriqueborrego@gmail.com>
2018-04-30 15:58:47 +0100

    create contact sensor plugin
    Added an example contact sensor plugin with minimal features.

---
+++ plugins/ContactSensorPlugin.cc
@@ -0,0 +1,119 @@
+/*!
+    \file plugins/ContactSensorPlugin.cc
+    \brief ContactSensor Gazebo plugin
+
+    Plugin for handling contact sensors
+
+    \author João Borrego : jsbruglie
+*/
+
+#include "ContactSensorPlugin.hh"
+
+namespace gazebo {
+
+
+/// \brief Class for private ContactSensor plugin data.
+class ContactSensorPluginPrivate
+{
+    /// Gazebo transport node
+    public: transport::NodePtr node;
+    /// Gazebo topic subscriber
+    public: transport::SubscriberPtr sub;
+    /// Gazebo topic publisher
+    public: transport::PublisherPtr pub;
+
+    /// Mutex for safe data access
+    public: std::mutex mutex;
+};
+
+// Register this plugin with the simulator
+GZ_REGISTER_SENSOR_PLUGIN(ContactSensorPlugin)
+
+/////////////////////////////////////////////////
+ContactSensorPlugin::ContactSensorPlugin() : SensorPlugin(),
+    data_ptr(new ContactSensorPluginPrivate)
+{
+    gzmsg << "[ContactSensorPlugin] Loaded plugin." << std::endl;
+}
+
+/////////////////////////////////////////////////
+ContactSensorPlugin::~ContactSensorPlugin()
+{
+    gzmsg << "[ContactSensorPlugin] Unloaded plugin." << std::endl;
+}
+
+/////////////////////////////////////////////////
+void ContactSensorPlugin::Load(sensors::SensorPtr _sensor, sdf::ElementPtr _sdf)
+{
+    this->sensor = std::dynamic_pointer_cast<sensors::ContactSensor>(_sensor);
+
+    // Make sure the parent sensor is valid
+    if (!this->sensor)
+    {
+        gzerr << "No contact sensor provided." << std::endl ;
+        return;
+    }
+
+    // Connect to the sensor update event
+    this->update_connection = this->sensor->ConnectUpdated(
+      std::bind(&ContactSensorPlugin::onUpdate, this));
+    // Make sure the parent sensor is active
+    this->sensor->SetActive(true);
+
+    // Setup transport node
+    this->data_ptr->node = transport::NodePtr(new transport::Node());
+    this->data_ptr->node->Init();
+    // Subcribe to the monitored requests topic
+    this->data_ptr->sub = this->data_ptr->node->Subscribe(REQUEST_TOPIC,
+        &ContactSensorPlugin::onRequest, this);
+    // Setup publisher for the response topic
+    this->data_ptr->pub = this->data_ptr->node->
+        Advertise<ContactSensorResponse>(RESPONSE_TOPIC);
+}
+
+/////////////////////////////////////////////////
+void ContactSensorPlugin::onUpdate()
+{
+    // TODO
+}
+
+/////////////////////////////////////////////////
+void ContactSensorPlugin::onRequest(ContactSensorRequestPtr &_msg)
+{
+    ContactSensorResponse msg;
+
+    if (_msg->has_type()) {
+
+        int type = _msg->type();
+        if (type == REQ_IN_CONTACT)
+        {
+            if (_msg->has_in_contact()) {
+                std::string name = _msg->in_contact();
+                msg.set_type(RES_IN_CONTACT);
+                if (this->inContact(name)) {
+                    msg.set_in_contact(name);
+                }
+                this->data_ptr->pub->Publish(msg);
+            }
+        }
+    }
+}
+
+/////////////////////////////////////////////////
+bool ContactSensorPlugin::inContact(std::string & name)
+{
+    // Get all the contacts
+    msgs::Contacts contacts;
+    contacts = this->sensor->Contacts();
+    // Check each of the contacts for a collision matching given name
+    for (unsigned int i = 0; i < contacts.contact_size(); ++i)
+    {
+        std::string link_name = contacts.contact(i).collision2();
+        if (link_name.find(name) != std::string::npos) {
+            return true;
+        }
+    }
+    return false;
+}
+
+}
---
+++ plugins/ContactSensorPlugin.hh
@@ -0,0 +1,95 @@
+/*!
+    \file plugins/ContactSensorPlugin.hh
+    \brief ContactSensor Gazebo plugin
+
+    Plugin for handling contact sensors
+
+    \author João Borrego : jsbruglie
+*/
+
+#ifndef _CONTACT_SENSOR_PLUGIN_HH_
+#define _CONTACT_SENSOR_PLUGIN_HH_
+
+// Gazebo
+#include "gazebo/common/Plugin.hh"
+#include <gazebo/msgs/msgs.hh>
+#include <gazebo/transport/transport.hh>
+#include <gazebo/sensors/sensors.hh>
+
+// Custom messages
+#include "contact_sensor_request.pb.h"
+#include "contact_sensor_response.pb.h"
+
+namespace ContactSensorPlugin {
+
+    // Plugin messages
+
+    /// Topic for incoming requests
+    #define REQUEST_TOPIC   "~/grasp/contact"
+     /// Topic for outgoing responses
+    #define RESPONSE_TOPIC  "~/grasp/contact/response"
+
+    /// Request for test contact
+    #define REQ_IN_CONTACT   grasp::msgs::ContactSensorRequest::IN_CONTACT
+
+    /// Response for test contact
+    #define RES_IN_CONTACT   grasp::msgs::ContactSensorResponse::IN_CONTACT
+}
+
+namespace gazebo {
+
+    /// Declaration for request message type
+    typedef grasp::msgs::ContactSensorRequest ContactSensorRequest;
+    /// Shared pointer declaration for request message type
+    typedef const boost::shared_ptr<const grasp::msgs::ContactSensorRequest>
+        ContactSensorRequestPtr;
+    /// Declaration for response message type
+    typedef grasp::msgs::ContactSensorResponse ContactSensorResponse;
+    /// Shared pointer declaration for response message type
+    typedef const boost::shared_ptr<const grasp::msgs::ContactSensorResponse>
+        ContactSensorResponsePtr;
+
+    // Forward declaration of private data class
+    class ContactSensorPluginPrivate;
+
+    // TODO
+    class ContactSensorPlugin : public SensorPlugin
+    {
+        // Private attributes
+
+        /// Class with private attributes
+        private: std::unique_ptr<ContactSensorPluginPrivate> data_ptr;
+        /// Model to which the plugin is attached
+        private: sensors::ContactSensorPtr sensor;
+        /// Connection to world update event
+        private: event::ConnectionPtr update_connection;
+
+        // Public methods
+
+        /// \brief Constructs the object
+        public: ContactSensorPlugin();
+
+        /// \brief Destroys the object
+        public: virtual ~ContactSensorPlugin();
+
+        /// \brief Loads the plugin
+        /// \param _model The sensor pointer
+        /// \param _sdf   The sdf element pointer
+        public: virtual void Load(sensors::SensorPtr _sensor, sdf::ElementPtr _sdf);
+
+        /// \brief Updates model on world update
+        public: void onUpdate();
+
+        /// \brief Callback function for handling incoming requests
+        /// \param _msg  The message
+        public: void onRequest(ContactSensorRequestPtr & _msg);
+
+        /// \brief Returns if sensor is in contact with given collision model
+        /// \param name Name of collision to test
+        /// \return Whether sensor is in contact with collision model
+        private: bool inContact(std::string & name);
+
+    };
+}
+
+#endif
