935c653c1152925d71d5a0838600e2ca3b97ae72
Borrego <joaohenriqueborrego@gmail.com>
2018-09-19 12:50:30 +0100

    add proper structure to randomiser
    Created abstract classes for uniform handling of different random properties

--- grasp/Randomiser.cc
+++ grasp/Randomiser.cc
@@ -15,30 +15,37 @@ Randomiser::Randomiser(const std::string & config):
 }
 
 //////////////////////////////////////////////////
-GaussianProperty::GaussianProperty(
-    const std::string & property_, double mean_,
-    double std_, double scale_):
-        property(property_), mean(mean_), std(std_), scale(scale_), dist(mean_, std_)
+void RandomProperty::fillMsg(DRRequest & msg)
+{
+    return;
+}
+
+//////////////////////////////////////////////////
+double RandomSampler::sample(std::mt19937 & gen)
+{
+    return 0;
+}
+
+//////////////////////////////////////////////////
+GaussianSampler::GaussianSampler(double mean_, double std_):
+    mean(mean_), std(std_), dist(mean_, std_)
 {
 }
 
 //////////////////////////////////////////////////
-double GaussianProperty::sample(std::mt19937 & gen)
+double GaussianSampler::sample(std::mt19937 & gen)
 {
     return dist(gen);
 }
 
 //////////////////////////////////////////////////
-UniformProperty::UniformProperty(
-    const std::string & property_, double a_,
-    double b_, double scale_, bool log_uniform_):
-        property(property_), a(a_), b(b_), scale(scale_), dist(a_, b_),
-        log_uniform(log_uniform_)
+UniformSampler::UniformSampler(double a_, double b_, bool log_uniform_):
+    a(a_), b(b_), dist(a_, b_), log_uniform(log_uniform_)
 {
 }
 
 //////////////////////////////////////////////////
-double UniformProperty::sample(std::mt19937 & gen)
+double UniformSampler::sample(std::mt19937 & gen)
 {
     if (log_uniform) {
         return std::exp(dist(gen));
--- grasp/Randomiser.hh
+++ grasp/Randomiser.hh
@@ -24,30 +24,34 @@ typedef gap::msgs::DRRequest DRRequest;
 /// Declaration for model command message type
 typedef gap::msgs::ModelCmd ModelCmdMsg;
 
-/// \brief Physics property with gaussian disturbance
-class GaussianProperty
+// Random samplers
+
+/// \brief Abstract PRNG sampler class
+class RandomSampler
 {
-    /// \brief Property name
-    public: std::string property;
-    /// \brief Gaussian mean
-    public: double mean;
-    /// \brief Gaussian standard deviation
-    public: double std;
-    /// \brief Scale factor
-    public: double scale;
+    /// \brief Gets random sample
+    /// \param gen Pseudo random number generator
+    /// \returns Random sample
+    public: virtual double sample(std::mt19937 & gen);
+};
+
+/// \brief Gaussian distribution sampler
+class GaussianSampler : RandomSampler
+{
     /// \brief Gaussian distribution
     public: std::normal_distribution<double> dist;
 
+    /// \brief Gaussian mean
+    private: double mean;
+    /// \brief Gaussian standard deviation
+    private: double std;
+
     /// \brief Constructor
-    /// \param property Property name
     /// \param mean Gaussian mean
     /// \param std Gaussian standard deviation
-    /// \param scale Scaling factor
-    public: GaussianProperty(
-        const std::string & property,
+    public: GaussianSampler(
         double mean,
-        double std,
-        double scale=0.0);
+        double std);
 
     /// \brief Gets random sample
     /// \param gen Pseudo random number generator
@@ -55,33 +59,26 @@ class GaussianProperty
     public: double sample(std::mt19937 & gen);
 };
 
-/// \brief Physics property with uniform disturbance
-class UniformProperty
+/// \brief Uniform/Log-uniform distribution sampler
+class UniformSampler : RandomSampler
 {
-    /// \brief Property name
-    public: std::string property; 
-    /// \brief Uniform lower limit
-    public: double a;
-    /// \brief Uniform higher limit
-    public: double b;
-    /// \brief Scale factor
-    public: double scale;
     /// \brief Uniform distribution
     public: std::uniform_real_distribution<double> dist;
-
+ 
+    /// \brief Uniform lower limit
+    private: double a;
+    /// \brief Uniform higher limit
+    private: double b;
     /// \brief Whether to use log-uniform instead
-    public: bool log_uniform {false};
+    private: bool log_uniform {false};
 
     /// \brief Constructor
-    /// \param property Property name
     /// \param mean Uniform mean
     /// \param std Uniform standard deviation
-    /// \param scale Scaling factor
-    public: UniformProperty(
-        const std::string & property,
+    /// \param log_uniform Whether to use log-uniform or uniform dist
+    public: UniformSampler(
         double a,
         double b,
-        double scale=0.0,
         bool log_uniform=false);
 
     /// \brief Gets random sample
@@ -90,12 +87,142 @@ class UniformProperty
     public: double sample(std::mt19937 & gen);
 };
 
+// Random properties
+
+/// \brief Abstract class for random property
+class RandomProperty
+{
+    /// \brief Random distribution sampler
+    private: RandomSampler sampler;
+    /// \brief Whether term is additive or a scaling factor
+    private: bool additive;
+    /// \brief Model scale
+    private: double scale; 
+
+    /// \brief TODO
+    public: virtual void fillMsg(DRRequest & msg);
+};
+
+/// \brief Model scale random property
+class ModelScale : RandomProperty
+{
+    /// \brief Constructor
+    /// TODO params
+    public: ModelScale(
+        RandomSampler & sampler_,
+        bool additive_,
+        double scale_,
+        std::vector<std::string> & models_);
+
+    /// \brief TODO
+    public: void fillMsg(DRRequest & msg);
+};
+
+/// \brief Link mass random property
+class LinkMass : RandomProperty
+{
+    /// \brief Constructor
+    /// TODO params
+    public: LinkMass(
+        RandomSampler & sampler_,
+        bool additive_,
+        double scale_,
+        std::vector<std::string> & links_,
+        std::vector<double> & masses_);
+
+    /// \brief TODO
+    public: void fillMsg(DRRequest & msg);
+};
+
+/// \brief Friction coefficients random property
+class FrictionCoefficient : RandomProperty
+{
+    /// \brief Constructor
+    /// TODO params
+    public: FrictionCoefficient(
+        RandomSampler & sampler_,
+        bool additive_,
+        double scale_,
+        std::vector<std::string> & links_,
+        std::vector<double> & mu1_,
+        std::vector<double> & mu2_,
+        std::vector<double> & kp_,
+        std::vector<double> & kd_);
+
+    /// \brief TODO
+    public: void fillMsg(DRRequest & msg);
+};
+
+/// \brief Joint damping coefficients random property
+class JointDampingCoefficient : RandomProperty
+{
+    /// \brief Constructor
+    /// TODO params
+    public: JointDampingCoefficient(
+        RandomSampler & sampler_,
+        bool additive_,
+        double scale_,
+        std::vector<std::string> & joints_,
+        std::vector<double> & damping_);
+
+    /// \brief TODO
+    public: void fillMsg(DRRequest & msg);
+};
+
+/// \brief P controller gains random property
+class PGain : RandomProperty
+{
+    /// \brief Constructor
+    /// TODO params
+    public: PGain(
+        RandomSampler & sampler_,
+        bool additive_,
+        double scale_,
+        std::vector<std::string> & joints_,
+        std::vector<double> & p_gains_);
+
+    /// \brief TODO
+    public: void fillMsg(DRRequest & msg);
+};
+
+/// \brief Joint limits random property
+class JointLimit : RandomProperty
+{
+    /// \brief Constructor
+    /// TODO params
+    public: JointLimit(
+        RandomSampler & sampler_,
+        bool additive_,
+        double scale_,
+        std::vector<std::string> & joints_,
+        std::vector<double> & lower_,
+        std::vector<double> & upper_);
+
+    /// \brief TODO
+    public: void fillMsg(DRRequest & msg);
+};
+
+/// \brief Joint limits random property
+class Gravity : RandomProperty
+{
+    /// \brief Constructor
+    /// TODO params
+    public: Gravity(
+        RandomSampler & sampler_,
+        bool additive_,
+        double scale_,
+        std::vector<double> gravity_);
+
+    /// \brief TODO
+    public: void fillMsg(DRRequest & msg);
+};
+
+
+// Randomiser class
+
 /// \brief Randomiser representation class
 class Randomiser
 {
-
-    // Public attributes
-
     // Private attributes
 
     /// Mersenne Twister pseudorandom number generator
@@ -104,19 +231,8 @@ class Randomiser
     /// DRInterface API
     private: DRInterface api;
 
-    /// Gaussian properties
-    private: std::vector<GaussianProperty> n_p;
-    /// Uniform properties
-    private: std::vector<UniformProperty> u_p;
-
     /// \brief Constructor
     public: Randomiser(const std::string & config);
-
-    /// TODO
-    private: void ProcessModelScale(
-        DRRequest & msg,
-        const std::string & model,
-        const double scale);
 };
 
 #endif
