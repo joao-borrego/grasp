b33246d5dbc261b6aecf75382df413a627ab6a10
Borrego <joaohenriqueborrego@gmail.com>
2018-09-19 14:56:38 +0100

    gap version bump to 1.3

--- setup.sh
+++ setup.sh
@@ -4,9 +4,9 @@
 source /usr/local/share/gazebo-9/setup.sh
 
 # Export location to GAP 1.2 plugins
-export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:/usr/local/lib/gap-1.2
+export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:/usr/local/lib/gap-1.3
 # Export GAP message library
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib/gap-1.2
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib/gap-1.3
 
 # Export location of grasp plugins
 export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:`pwd`/build/plugins
